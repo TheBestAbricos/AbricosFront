55b895f698a478a66c4c257082ebe15548fe1aa9
Ян Козыренко <kopn48@mail.ru>
2022-07-05 22:54:42 +0300

    add auth

---
+++ src/components/AuthForm.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+
+</script>
+
+<div class="container">
+    <div class="form_container">
+
+    </div>
+</div>
+
+
+
+<style>
+    .container {
+        height: 100vh;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        padding: 20px;
+    }
+    .form_container {
+        width: 400px;
+        height: 400px;
+        background-color: #ccc;
+    }
+</style>
---
+++ src/routes/login.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+    import { createAccount } from "$lib/firebase";
+    import AuthForm from "../components/AuthForm.svelte";
+    import { prevent_default } from "svelte/internal";
+    export let email: string; 
+    export let password: string; 
+
+    const createAccountButtinClick = async (e: MouseEvent) => {
+        console.log(e);
+        createAccount(email, password);
+    }
+</script>
+
+<!-- 
+<form action="">
+    <input type="text" name="" id="" bind:value={email}>
+    <input type="text" name="" id="" bind:value={password}>
+    <button type="button" on:click={createAccountButtinClick}>Create user</button>
+</form> -->
+
+<AuthForm />
+
+<style>
+
+
+</style>
