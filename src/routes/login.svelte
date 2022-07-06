9c2cb47203dfea1abf5ffd158357d24959c2c4c7
Ян Козыренко <kopn48@mail.ru>
2022-07-06 15:49:00 +0300

    add authentication screen, redirection

--- src/routes/login.svelte
+++ src/routes/login.svelte
@@ -1,24 +1,24 @@
 <script lang="ts">
-    import { createAccount } from "$lib/firebase";
+    import { createAccount, getCurrentUser } from "$lib/firebase";
     import AuthForm from "../components/AuthForm.svelte";
     import { prevent_default } from "svelte/internal";
-    export let email: string; 
-    export let password: string; 
-
+    let email: string; 
+    let password: string; 
     const createAccountButtinClick = async (e: MouseEvent) => {
         console.log(e);
         createAccount(email, password);
     }
 </script>
 
-<!-- 
-<form action="">
-    <input type="text" name="" id="" bind:value={email}>
-    <input type="text" name="" id="" bind:value={password}>
-    <button type="button" on:click={createAccountButtinClick}>Create user</button>
-</form> -->
-
-<AuthForm />
+{#await getCurrentUser()}
+    
+{:then user} 
+    {#if !user}
+        <AuthForm isRegister={false} />
+        {:else}
+        {window.location.href = "/"}
+    {/if}
+{/await}
 
 <style>
 
