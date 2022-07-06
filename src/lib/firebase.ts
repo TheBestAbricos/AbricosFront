d8da649d33a3c594ad4016884669a60efc87586f
Ян Козыренко <kopn48@mail.ru>
2022-07-06 23:09:14 +0300

    ci/cd

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,11 +3,14 @@ import {
     getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, setPersistence, browserSessionPersistence,
     signOut, type UserCredential, type User
 } from 'firebase/auth';
-
+import * as fs from "firebase/firestore" 
 const firebaseConfig = {
-    apiKey: 'AIzaSyBzusLOWN7a_sSCbm2TvzO0G3rkNGDnYlE',
-    authDomain: 'project-1007319276577.firebaseapp.com',
-    projectId: 'abicos',
+    apiKey: "AIzaSyBzusLOWN7a_sSCbm2TvzO0G3rkNGDnYlE",
+    authDomain: "abicos.firebaseapp.com",
+    projectId: "abicos",
+    storageBucket: "abicos.appspot.com",
+    messagingSenderId: "1007319276577",
+    appId: "1:1007319276577:web:3ae3d6e77fbbe2779a9760"
 };
 
 let isSetPersistance : boolean;
@@ -15,11 +18,14 @@ let isSetPersistance : boolean;
 initializeApp(firebaseConfig);
 
 const auth = getAuth();
+const firestore = fs.getFirestore();
 
 const initPersistance = async () : Promise<void> => {
     await setPersistence(auth, browserSessionPersistence);
     isSetPersistance = true;
 }
+
+
 
 export const createAccount = async (email: string, password: string) : Promise<UserCredential> => {
     const userCredentaials = await createUserWithEmailAndPassword(auth, email, password);
