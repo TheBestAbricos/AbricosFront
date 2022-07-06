84d6b1b6e195d08209093b5a82a2a596ac596c41
system205 <arsenmutalapov@gmail.com>
2022-07-06 14:30:03 +0300

    add card ant interface for it

--- src/components/Card.svelte
+++ src/components/Card.svelte
@@ -1,26 +1,34 @@
 <script lang="ts">
-	export let text = 'Make a dinner today and then...';
-	export let time = '01:22';
-	export let date = '02.02.2003';
-	export let checked = false;
-	export let tags = ['tag', 'tag2'];
+	import type { Card } from '../types/card';
+	export const card: Card = {
+		text: 'Make a dinner today and then then then then then today',
+		tags: [
+			{ text: 'must', color: '#ff0000' },
+			{ text: 'should', color: '#00FF00' },
+			{ text: 'really woolddldl', color: '#0000ff' },
+			{ text: 're123ally wooldl', color: '#0000ff' }
+		],
+		checked: true,
+		date: '02.02.2003',
+		time: '01:22'
+	};
 </script>
 
 <div class="card">
 	<header>
 		<div class="date-div">
-			<div class="date">{date}</div>
-			<div class="time">{time}</div>
+			<div class="date">{card.date}</div>
+			<div class="time">{card.time}</div>
 		</div>
 		<div class="image"><img alt="delete" src="images/x.png" /></div>
 	</header>
 	<main>
-		<p>{text}</p>
-		<span for=""><input type="checkbox" name="checked" {checked} /></span>
+		<p>{card.text}</p>
+		<span for=""><input type="checkbox" name="checked" checked={card.checked} /></span>
 	</main>
 	<footer>
-		{#each tags as tag}
-			<div class="tag">{tag}</div>
+		{#each card.tags as tag}
+			<div class="tag" style="background-color:{tag.color}">{tag.text}</div>
 		{/each}
 	</footer>
 </div>
@@ -31,62 +39,86 @@
 		justify-content: space-between;
 		border-bottom: 1px solid grey;
 		box-sizing: border-box;
+		flex-basis: 2rem;
 	}
 
 	.date-div {
-		padding: 5px;
-		font-size: 20px;
+		padding: 0.3125rem;
+		font-size: 1.25rem;
+		line-height: 1.1;
 		text-align: center;
-		margin: 0 15px;
+		margin: 0 1rem;
 	}
 
 	.image {
 		display: flex;
 		justify-content: center;
 		align-items: center;
-		padding: 5px;
-		margin: 0 10px;
+		margin: 0 1rem;
 	}
 	img {
-		height: 32px;
+		height: 2rem;
 	}
 	main {
 		display: flex;
-		flex-grow: 5;
+		flex-basis: 6rem;
+		overflow: hidden;
 	}
 	p {
-		padding: 5px;
-		font-size: 22px;
+		padding: 0.5rem 0.5rem 0 1rem;
+		font-size: 1.3rem;
+		line-height: 1.2;
 		flex-grow: 1;
 		margin: 0;
 	}
 	span {
-		padding: 5px;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		margin-right: 1rem;
 	}
 	input[type='checkbox'] {
 		cursor: pointer;
-		height: 30px;
-		width: 30px;
+		height: 2rem;
+		width: 2rem;
+	}
+	input[type='checkbox']:hover {
+		background-color: aqua;
 	}
 	footer {
 		box-sizing: border-box;
-		padding: 0 10px;
+		flex-basis: 3rem;
+		overflow: hidden;
+		padding: 0 1rem;
 	}
 	.tag {
-		height: 30px;
-		min-width: 40px;
+		height: 2rem;
+		min-width: 3rem;
 		text-align: center;
-		margin: 5px;
-		box-sizing: border-box;
+		padding: 0.5rem;
+		margin: 0.5rem 0;
+		display: inline-flex;
+		justify-content: center;
+		align-items: center;
 		border: 1px solid grey;
 		border-radius: 10px;
-		display: inline-block;
+		box-sizing: border-box;
+		flex-wrap: nowrap;
+		transition-duration: 300ms;
+		font-weight: 500;
+	}
+	.tag:hover {
+		transform: scale(1.02);
+		transform: rotate(-1deg);
+	}
+	.tag:not(:last-child) {
+		margin-right: 0.2rem;
 	}
 	.card {
 		display: flex;
 		flex-direction: column;
-		width: 300px;
-		height: 200px;
+		width: 18.75rem;
+		height: 12rem;
 		border: 1px solid grey;
 		border-radius: 30px;
 		box-sizing: border-box;
---
+++ src/types/card.ts
@@ -0,0 +1,12 @@
+export interface Card {
+    text: string,
+    tags: Tag[],
+    time: string,
+    date: string,
+    checked: boolean,
+}
+
+interface Tag {
+    text: string,
+    color: string,
+}
