d6afe2235bdc9ea202a29b33f5a368167d3f6a00
macoyshev <make.oynoshev@gmail.com>
2022-07-07 12:02:12 +0300

    feat: add toggle, cancel-button, save-button shared componentes

---
+++ src/components/shared/SaveButton.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+
+</script>
+
+<button on:click>
+    Save
+</button>
+
+<style>
+   button {
+    height: 30px;
+    width: 90px;
+
+    border-radius: 30px;
+    border: 1px solid rgba(0, 0, 0, 28%);
+
+    background: #93D6F2;
+   } 
+</style>
