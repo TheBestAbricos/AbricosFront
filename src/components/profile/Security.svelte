6cc2f8216d69b62958a4f505c3a43ee7f837d3e8
m.oynoshev <m.oynoshev@tinkoff.ru>
2022-07-08 21:05:23 +0300

    Add logo change

---
+++ src/components/profile/Security.svelte
@@ -0,0 +1,18 @@
+<script lang='ts'>
+
+</script>
+<div class='container'>
+    <p>Will be added later</p>
+</div>
+
+<style>
+    .container  {
+        width: 100%;
+        height: 100%;
+
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+    }
+</style>
