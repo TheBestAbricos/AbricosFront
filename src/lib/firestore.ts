b34fd27b7eb5516b4d80a67ff429f958110e6346
Ян Козыренко <kopn48@mail.ru>
2022-07-08 22:36:50 +0300

    add checkbox changing

--- src/lib/firestore.ts
+++ src/lib/firestore.ts
@@ -40,6 +40,13 @@ export async function getCardsInCurrentFolder() : Promise<Card[]> {
         else return 0;
     });
 }
+export async function changeChecked(docId: string, checked: boolean) {
+    const currentFolderId = (await getCurrentUserInfo()).currentFolder;
+    const itemsCollection = fs.collection(fb.firestore, 'users', fb.getCurrentUser().uid, "folders",
+        currentFolderId, "items");
+
+    fs.updateDoc(fs.doc(itemsCollection, docId), {checked});
+} 
 export async function updateCardInFolder(folderName: string, card: Card) : Promise<void> {
     const currentFolderId = (await getCurrentUserInfo()).currentFolder;
 
