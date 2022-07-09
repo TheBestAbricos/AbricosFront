08141591c585d1d70f4f063d247032cf77daf1af
system205 <arsenmutalapov@gmail.com>
2022-07-09 13:40:34 +0300

    firestore format

--- src/lib/firestore.ts
+++ src/lib/firestore.ts
@@ -1,44 +1,44 @@
-import * as fb from "$lib/firebase"
-import * as fs from "firebase/firestore"; 
+import * as fs from "firebase/firestore";
 import { Timestamp } from "firebase/firestore";
-import type {Card}  from "./types/card";
+import * as fb from "$lib/firebase"
+import type { Card } from "./types/card";
 import type { Folder } from "./types/folder";
 import type { FirestoreUser } from "./types/user";
 
 
-export async function getCurrentUserInfo() : Promise<FirestoreUser> {
+export async function getCurrentUserInfo(): Promise<FirestoreUser> {
     return (await fs.getDoc(fs.doc(fb.firestore, "users", fb.getCurrentUser().uid))).data() as FirestoreUser;
 }
 
-export async function getCardsInCurrentFolder() : Promise<Card[]> {
+export async function getCardsInCurrentFolder(): Promise<Card[]> {
 
     const currentFolderId = (await getCurrentUserInfo()).currentFolder;
-    
+
     const itemsCollection = fs.collection(fb.firestore, 'users', fb.getCurrentUser().uid, "folders",
         currentFolderId, "items");
 
     const snapshot = await fs.getDocs(
         fs.query(itemsCollection)
     );
-    const cards : Card[] = [];
+    const cards: Card[] = [];
     snapshot.docs.forEach((e) => {
         const document = e.data() as Card;
         cards.push(document);
     });
     return cards.sort((a: Card, b: Card) => {
-        if(!a.date){
+        if (!a.date) {
             return 1;
         }
-        if(!b.date){
+        if (!b.date) {
             return -1
         }
-        if(a.date > b.date) {
+        if (a.date > b.date) {
             return 1;
         }
-        if(a.date < b.date) {
+        if (a.date < b.date) {
             return -1;
         }
-        else return 0;
+        return 0;
     });
 }
 export async function changeChecked(docId: string, checked: boolean) {
@@ -46,31 +46,29 @@ export async function changeChecked(docId: string, checked: boolean) {
     const itemsCollection = fs.collection(fb.firestore, 'users', fb.getCurrentUser().uid, "folders",
         currentFolderId, "items");
 
-    fs.updateDoc(fs.doc(itemsCollection, docId), {checked});
-} 
-export async function updateCardInFolder(folderName: string, card: Card) : Promise<void> {
+    fs.updateDoc(fs.doc(itemsCollection, docId), { checked });
+}
+export async function updateCardInFolder(folderName: string, card: Card): Promise<void> {
     const currentFolderId = (await getCurrentUserInfo()).currentFolder;
 
     const itemsCollection = fs.collection(fb.firestore, 'users', fb.getCurrentUser().uid, "folders",
         currentFolderId, "items");
-    if(card.docId){
-        const newDoc = fs.doc(itemsCollection, card.docId); 
-        fs.setDoc(newDoc, card, {merge: true});
+    if (card.docId) {
+        const newDoc = fs.doc(itemsCollection, card.docId);
+        fs.setDoc(newDoc, card, { merge: true });
     }
     else {
-        const newDoc = fs.doc(itemsCollection); 
+        const newDoc = fs.doc(itemsCollection);
         card.docId = newDoc.id;
-        fs.setDoc(newDoc, card, {merge: true});
+        fs.setDoc(newDoc, card, { merge: true });
     }
-    
+
 }
-export async function getAllUserFolders() : Promise<Folder[]> {
+export async function getAllUserFolders(): Promise<Folder[]> {
     const foldersCollection = fs.collection(fb.firestore, 'users', fb.getCurrentUser().uid, "folders");
 
     const foldersDocs = await fs.getDocs(foldersCollection);
-    return foldersDocs.docs.map((e) => {
-        return (e.data() as Folder);
-    }) as Folder[];
+    return foldersDocs.docs.map((e) => (e.data() as Folder)) as Folder[];
 }
 export async function deleteCard(docId: string): Promise<void> {
 
@@ -83,7 +81,7 @@ export async function deleteCard(docId: string): Promise<void> {
 export async function getCardByDocId(docId: string): Promise<Card> {
 
     const currentFolderId = (await getCurrentUserInfo()).currentFolder;
-    
+
     const itemsCollection = fs.collection(fb.firestore, 'users', fb.getCurrentUser().uid, "folders",
         currentFolderId, "items");
 
@@ -93,23 +91,23 @@ export async function getCardByDocId(docId: string): Promise<Card> {
 export async function updateFolder(folder: Folder): Promise<string> {
     const foldersCollection = fs.collection(fb.firestore, 'users', fb.getCurrentUser().uid, "folders");
     folder.creationDate = Timestamp.fromMillis(Date.now());
-    if (folder.docId) {
+    if (!folder.docId) {
         const folderDoc = fs.doc(foldersCollection);
         folder.docId = folderDoc.id;
-        await fs.setDoc(folderDoc, folder, {merge: true}); 
+        await fs.setDoc(folderDoc, folder, { merge: true });
         return folder.docId;
     }
-    else {
-        const folderDoc = fs.doc(foldersCollection, folder.docId);
-        await fs.setDoc(folderDoc, folder, {merge: true});
-        return folder.docId as string;
-    }
+
+    const folderDoc = fs.doc(foldersCollection, folder.docId);
+    await fs.setDoc(folderDoc, folder, { merge: true });
+    return folder.docId as string;
+
 
 }
 export async function setNotificationToken(token: string): Promise<void> {
     const userCollection = fs.collection(fb.firestore, 'users');
     const userDoc = fs.doc(userCollection, fb.getCurrentUser().uid);
-    await fs.updateDoc(userDoc, {token: token});
+    await fs.updateDoc(userDoc, { token });
 }
 export async function getNotificationToken(): Promise<string | undefined> {
     const userCollection = fs.collection(fb.firestore, 'users');
@@ -119,7 +117,7 @@ export async function getNotificationToken(): Promise<string | undefined> {
 }
 export async function switchFolder(docId: string): Promise<void> {
     const userDoc = fs.doc(fs.collection(fb.firestore, 'users'), fb.getCurrentUser().uid);
-    fs.updateDoc(userDoc, {currentFolder: docId});
+    fs.updateDoc(userDoc, { currentFolder: docId });
 }
 export async function deleteFolder(docId: string): Promise<void> {
     const foldersCollection = fs.collection(fb.firestore, 'users', fb.getCurrentUser().uid, "folders");
