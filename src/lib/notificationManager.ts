1edcf3e3c966d0d519e3f72a1e4f93bb225fb230
anvar <anvartar2002@gmail.com>
2022-07-11 01:23:30 +0300

    notifications

--- src/lib/notificationManager.ts
+++ src/lib/notificationManager.ts
@@ -1,9 +1,9 @@
 // eslint-disable-next-line import/no-unresolved, import/extensions
 import { getAllUserCards, getNotificationToken, setNotificationToken } from '$lib/firestore';
 
-// export const urlServer = 'https://a321-188-130-155-167.eu.ngrok.io/';
+// export const urlServer = 'http://127.0.0.1:8000/';
 export const urlServer = 'https://852e-188-130-155-148.eu.ngrok.io/';
-// export const urlServer = 'https://5e8b-188-130-155-154.eu.ngrok.io/';
+// export const urlServer = 'https://a321-188-130-155-167.eu.ngrok.io/';
 
 export async function deleteNotification(docId: string) {
 	const token: string | undefined = await getNotificationToken();
@@ -17,7 +17,6 @@ export async function deleteNotification(docId: string) {
 		};
 		const res = await fetch(`${urlServer}webhooks/deleteSchedule/`, {
 			method: 'DELETE',
-			mode: "no-cors",
 			body: JSON.stringify(deleteData),
 			headers: {
 				'Content-Type': 'application/json'
@@ -38,50 +37,48 @@ export async function setNotification(datetime: string, docId: string, descripti
 		const updateData = {
 			time,
 			taskID: docId,
-			description: description,
+			description,
 			token: parseInt(token, 10)
 		};
 
-		const res = await fetch(`${urlServer}webhooks/schedule/`, {
+		await fetch(`${urlServer}webhooks/schedule/`, {
 			method: 'POST',
-			mode: "no-cors",
 			body: JSON.stringify(updateData),
 			headers: {
 				'Content-Type': 'application/json'
 			}
 		});
-		console.log(res.status);
+		// console.log(res.status);
 	}
 }
 
 export const sendTokenToTgBot = async (token: string) => {
 	// let token = token_input.value;
-	let response = await fetch(`${urlServer}webhooks/verifyToken/${token}/`, {
-		mode: "no-cors",
-	});
+	const response = await fetch(`${urlServer}webhooks/verifyToken/${token}/`);
+
+	// console.log(response.status);
 
 	if (!response.ok) {
+		// console.log(response.status);
 		throw Error('Invalid token');
 	}
 
 	await setNotificationToken(token);
 
 	const cards = await getAllUserCards();
-	// console.log(cards);
+
 	cards.forEach((card) => {
 		if (card.date && card.docId) {
-			let stringDate: string = new Date(card.date.seconds * 1000).toISOString();
+			const stringDate: string = new Date(card.date.seconds * 1000).toISOString();
 			setNotification(stringDate, card.docId, card.text);
-			console.log(card.docId, stringDate);
+			// console.log(card.docId, stringDate);
 		}
 	});
 };
 
 export const deleteTokenFromTgBot = async () => {
-	let token = await getNotificationToken();
+	const token = await getNotificationToken();
 
-	let res = await fetch(`${urlServer}webhooks/unlinkTelegram/${token}`, {
-		mode: "no-cors",
-	});
+	await fetch(`${urlServer}webhooks/unlinkTelegram/${token}`);
 	// console.log(res);
 };
